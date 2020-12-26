e05c97f87d77b5259852940e869c8953277ab96e
Александр Гончаренко <g.aleks80@gmail.com>
2020-12-26 23:11:18 +0500

    Дабавленый сервисные файлы часть 3

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+// const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("gulp-autoprefixer");
+const sync = require("browser-sync").create();
+
+const styles = () => {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    // .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    // .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(sync.stream());
+};
+
+const server = () => {
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+};
+
+const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
+};
+
+exports.default = gulp.series(
+  styles, server, watcher
+);
